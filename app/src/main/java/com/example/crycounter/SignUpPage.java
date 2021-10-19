a72770864f8263f84fbeaa53d0f71b9c1f770e68
Rheamukh <84300816+Rheamukh@users.noreply.github.com>
2021-10-19 08:17:12 -0500

    Created Firebase project for sign up page, followed LearnAuth example steps, currently adding styling to sign up xml file

--- app/src/main/java/com/example/crycounter/SignUpPage.java
+++ app/src/main/java/com/example/crycounter/SignUpPage.java
@@ -1,14 +1,76 @@
 package com.example.crycounter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class SignUpPage extends AppCompatActivity {
+    EditText emailET, passwordET;
+    TextView authStatusTV;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailET = findViewById(R.id.editTextEmail);
+        passwordET = findViewById(R.id.editTextPassword);
+        authStatusTV = findViewById(R.id.authStatusView);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            authStatusTV.setText("onStart reloaded and " + currentUser.getEmail() + " is logged in");
+            // Take any action needed here when screen loads and a user is logged in
+        }
+        else {
+            authStatusTV.setText("onStart reloaded and user is null");
+            // Take any action needed here when screen loads and a user is NOT logged in
+        }
+    }
+
+    public void signUp(String email, String password) {
+
+        // If the email and password passed in are not null, then try to create a User
+        if (email != null && password != null) {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.i("Denna", "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                authStatusTV.setText("Signed up " + user.getEmail() + " successfully");
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.i("Denna", "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(SignUpPage.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                authStatusTV.setText("Signed up - FAILED");
+                            }
+                        }
+                    });
+        }
     }
 }
--- app/src/main/java/com/example/crycounter/Themes.java
+++ app/src/main/java/com/example/crycounter/Themes.java
@@ -10,5 +10,6 @@ public class Themes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_themes);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
