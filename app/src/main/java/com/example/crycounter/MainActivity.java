710b129cddae03e533c6e13c0d8da7d9514e3f6b
andlanpo <andrewlanpouthakoun@gmail.com>
2021-10-19 08:15:04 -0500

    Added Nav Bar to go to Themes and Profile. also edited main activity

--- app/src/main/java/com/example/crycounter/MainActivity.java
+++ app/src/main/java/com/example/crycounter/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.crycounter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +17,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        if(menu instanceof MenuBuilder){
+            MenuBuilder m = (MenuBuilder) menu;
+            m.setOptionalIconsVisible(true);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.profile:
+                Intent intent1 = new Intent(this, Profile.class);
+                this.startActivity(intent1);
+                return true;
+            case R.id.themes:
+                Intent intent2 = new Intent(this, Themes.class);
+                this.startActivity(intent2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
