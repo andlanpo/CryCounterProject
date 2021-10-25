52272d80d7b4dadba9a3d75c837dd8c6081043c5
Rheamukh <84300816+Rheamukh@users.noreply.github.com>
2021-10-25 06:10:39 -0500

    finished signUpProfile

--- app/src/main/java/com/example/crycounter/SignUpProfile.java
+++ app/src/main/java/com/example/crycounter/SignUpProfile.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
 
 public class SignUpProfile extends AppCompatActivity {
 
@@ -15,6 +18,8 @@ public class SignUpProfile extends AppCompatActivity {
     private String firstName;
     private String lastName;
     private boolean privacy;
+    private ArrayList<String> stressors = new ArrayList<>();
+    private ArrayList<String> locations = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +49,29 @@ public class SignUpProfile extends AppCompatActivity {
                 break;
         }
     }
+
+    public void addStressor(View v){
+        EditText stressorText = (EditText)findViewById(R.id.addStressor);
+        String stressor = stressorText.getText().toString();
+        if(stressor.length() == 0){
+            stressorText.setError("Field cannot be left blank.");
+        }
+        stressors.add(stressor);
+        Toast.makeText(getApplicationContext(),"Added successfully",Toast.LENGTH_SHORT).show();
+        stressorText.setHint("Stressor");
+    }
+
+    public void addLocation(View v){
+        EditText locationText = (EditText)findViewById(R.id.addLocation);
+        String location = locationText.getText().toString();
+        if(location.length() == 0){
+            locationText.setError("Field cannot be left blank.");
+        }
+        locations.add(location);
+        Toast.makeText(getApplicationContext(),"Added successfully",Toast.LENGTH_SHORT).show();
+        locationText.setHint("Location");
+    }
+
 
     public void saveInfo(View v){
         EditText editFirstName = findViewById(R.id.editFirstName);
