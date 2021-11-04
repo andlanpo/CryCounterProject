dfe2c7a33666c6fcc39ab5a4733a9bae69667eec
andlanpo <andrewlanpouthakoun@gmail.com>
2021-11-04 08:15:15 -0500

    Almost done with leaderboard, only need to organize how it looks

--- app/src/main/java/com/example/crycounter/Leaderboard.java
+++ app/src/main/java/com/example/crycounter/Leaderboard.java
@@ -1,14 +1,44 @@
 package com.example.crycounter;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Leaderboard extends AppCompatActivity {
-
+    FireStoreHelper dbHelper;
+    private FirebaseFirestore db;
+    public static Profile profile;
+    FirebaseAuth mAuth;
+    Intent intent;
+    ArrayList<LeaderboardObject> leaderboardObjectArrayList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+        mAuth = FirebaseAuth.getInstance();
+        dbHelper = new FireStoreHelper();
+        db = FirebaseFirestore.getInstance();
+        leaderboardObjectArrayList = dbHelper.getLeaderboardObjects();
+        TextView text1 = findViewById(R.id.textView5);
+        text1.setText(leaderboardObjectArrayList.get(0).toString());
+
+
     }
 }
